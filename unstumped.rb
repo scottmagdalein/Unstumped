d68a796807ba2e73506c5843ebf675ad83ed0e33
Scott Magdalein <scottmagdalein@gmail.com>
2013-02-18 13:32:43 -0500

    updated with proper form and sending with Pony

--- unstumped.rb
+++ unstumped.rb
@@ -10,10 +10,6 @@ get '/' do
   haml :index
 end
 
-get '/apply' do
-	haml :apply
-end
-
 get '/pay' do
 	haml :pay
 end
@@ -27,36 +23,50 @@ get '/sadface' do
 end
 
 
-# post '/signup' do
-# 	@name = @user.name
+post '/apply' do
+	@name = params[name]
+	@email = params[email]
+	@phone = params[phone]
+	@what = params[what]
+	@experience = params[experience]
+	@links = params[links]
 
-# 	options = {
-# 		:to => "scottmagdalein@gmail.com",
-# 		:from => "no-reply@herokuapp.com",
-# 		:subject => "New user: #{@name}",
-# 		:body => "This is plain text.",
-# 		:via => :smtp,
-# 		:via_options => {
-# 			:address => 'smtp.mandrillapp.com',
-# 			:port => '587',
-# 			:enable_starttls_auto => true,
-# 			:user_name => 'scottmagdalein@gmail.com',
-# 			:password => '6xNf32J0tIlqgqW_epr3Kg',
-# 			:authentication => :plain,
-# 			:domain => "herokuapp.com"
-# 		}
-# 	}
-# end
+	haml :applicant
+
+	options = {
+		:to => "scottmagdalein@gmail.com",
+		:from => "no-reply@herokuapp.com",
+		:reply_to => "#{@email}"
+		:subject => "New applicant: #{@name}",
+		:html_body => haml(:applicant),
+		:via => :smtp,
+		:via_options => {
+			:address => 'smtp.mandrillapp.com',
+			:port => '587',
+			:enable_starttls_auto => true,
+			:user_name => 'scottmagdalein@gmail.com',
+			:password => '6xNf32J0tIlqgqW_epr3Kg',
+			:authentication => :plain,
+			:domain => "herokuapp.com"
+		}
+	}
+
+	Pony.mail(options)
+
+	haml :success
+end
 
 __END__
 @@layout
 !!!
 %html
 %head
-	%title Be a Web Developer with personal, 1-on-1 training
+	%title New Method - Become a Web Developer with personal, 1-on-1 training
 	%link{ :href => "/stylesheets/style.css", :type => "text/css", :rel => "stylesheet" }
 	%link{ :href => "/stylesheets/modal.css", :type => "text/css", :rel => "stylesheet" }
 	%link{ :href => "http://fonts.googleapis.com/css?family=Open+Sans:400,800,700|Merriweather:400,900", :rel => "stylesheet", :type => "text/css" }
 	%link{ :href => "/fonts/ss-symbolicons-block.css", :rel => "stylesheet", :type => "text/css" }
+	%script{ :src => "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"}
+	%script{ :src => "/scripts/jquery.scrollto.js"}
 %body
 	= yield
